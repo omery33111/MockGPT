cd7e406bdb4ac5e36fe60a9119da36158831bdff
omery33111 <omery33111@gmail.com>
2024-10-23 19:05:30 +0300

    Initializing

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import './config/dbConfig';
-import authRoutes from './routes/authenticationRoutes';
+import authenticationRoutes from './routes/authenticationRoutes';
 
 dotenv.config();
 
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 
 
-app.use('/user', authRoutes);
+app.use('/authentication', authenticationRoutes);
 
 
 
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Server is healthy' });
 });
 
-// הרצת השרת
+// Run Server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
